46f3c82ddac92f40854716f6b5943d9fe7516bf8
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-16 19:35:26 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace UltraChimps;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.3";
+    public const string Version = "1.1";
+    public const string Name = "UltraChimps";
+
+    public const string Description = "The Death Of You... Dont use double cash (Monkey knowledge still enable to make it possible also it may be impossible i didnt do testing so good luck)";
+
+    public const string RepoOwner = "Mattcy1"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "UltraChimps"; // TODO add your repo name here, also in the download url in README.md
+}
